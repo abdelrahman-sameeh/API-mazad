15d18fb4ac973507151c27b6d623deeaf23ecd86
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2023-10-13 15:04:58 +0300

    test hook

--- services/orderService.js
+++ services/orderService.js
@@ -61,13 +61,13 @@ exports.webhookCheckout = expressAsyncHandler(async (req, res, next) => {
 
   if (event.type === "checkout.session.completed") {
     //  Create order
+    console.log('test');
     const order = await Order.create({
       user: req.user._id,
       product: event.data.object.client_reference_id,
       price: event.data.object.amount_subtotal,
     });
     console.log(order);
-    console.log('test');
     if (order) {
       await Product.findByIdAndUpdate(event.data.object.client_reference_id, {
         isPaid: true,
