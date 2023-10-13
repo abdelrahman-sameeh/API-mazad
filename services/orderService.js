4ffdd55b879afd885c73047a280c2e81f36df25d
abdelrahman-sameeh <abdelrahman.sameeh507@gmail.com>
2023-10-13 17:45:21 +0300

    test

--- services/orderService.js
+++ services/orderService.js
@@ -76,9 +76,14 @@ exports.webhookCheckout = expressAsyncHandler(async (req, res, next) => {
         price: event.data.object.amount_subtotal,
       });
       await newOrder.save();
-      await Product.findByIdAndUpdate(event.data.object.data.productId, {
-        isPaid: true,
-      });
+      const product = await Product.findOneAndUpdate(
+        { _id: event.data.object.data.productId },
+        {
+          isPaid: true,
+        },
+        { new: true }
+      );
+      console.log(product);
     }
   }
 
